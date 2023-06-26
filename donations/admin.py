a89d55bd66d799319f86df357e92eeed17867ca7
daridev <darideveloper@gmail.com>
2023-06-26 11:36:21 -0600

    feat (admin): register ScreenLeaves model

--- donations/admin.py
+++ donations/admin.py
@@ -7,3 +7,10 @@ class MosaicAdmin(admin.ModelAdmin):
     list_per_page = 50
     search_fields = ('donor',)
     ordering = ('donor',)
+    
+@admin.register(models.ScreenLeaves)
+class ScreenLeavesAdmin(admin.ModelAdmin):
+    list_display = ('donor_frirst_name', 'donor_last_name')
+    list_per_page = 50
+    search_fields = ('donor_frirst_name', 'donor_last_name')
+    ordering = ('donor_frirst_name','donor_last_name')
